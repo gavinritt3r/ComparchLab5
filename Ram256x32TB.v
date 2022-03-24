c7391a7f976a7950a3853bc01b03ccbf43ea9ae0
gavinritt3r <60927156+gavinritt3r@users.noreply.github.com>
2022-03-24 11:38:00 -0400

    Add files via upload

--- Ram256x32TB.v
+++ Ram256x32TB.v
@@ -14,7 +14,7 @@ Ram256x32 Ram(addr,Din,Dout,RW,clk);
 integer i = 0;
 
 
-always #20 clk = ~clk;
+always #10 clk = ~clk;
 
 initial begin
 	{clk,RW,addr,Din} = 0;
@@ -23,17 +23,33 @@ initial begin
 
 	for (i=0; i < 2**8; i = i+1) begin
 		repeat (1) @(posedge clk) addr <= i; RW <= 1; Din <= $random;
-		
 	end
 
 	#40 RW <=0;
 
-	for (i=0; i < 2**8; i = i+1) begin
+	for (i=0; i < 64; i = i+1) begin
 		repeat (1) @(posedge clk) addr <= i; 
-		repeat (1) @(posedge clk);
 	end
+	
+	#20;
+	
+	for (i=64; i < 128; i = i+1) begin
+		repeat (1) @(posedge clk) addr <= i; 
+	end
+	
+	#20;
+	
+	for (i=128; i < 192; i = i+1) begin
+		repeat (1) @(posedge clk) addr <= i; 
+	end	
 
-	#20 $stop;
+	#20;
+	
+	for (i=192; i < 256; i = i+1) begin
+		repeat (1) @(posedge clk) addr <= i; 
+	end
+	
+	#200 $stop;
 
 	end
 endmodule
